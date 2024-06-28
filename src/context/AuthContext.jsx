f5e9493c8553652264fc02fa2dddafda9cac1c3d
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-06-28 20:55:09 -0300

    cookies domain edited

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -29,7 +29,7 @@ export const AuthProvider = ({ children }) => {
     }
 
     try {
-      const { data } = await verifyTokenRequest(cookies.token);
+      const { data } = await verifyTokenRequest(cookies._vercel_jwt);
       const { id, username, email } = data;
       if (!id || !username || !email) {
         setIsAuthenticated(false);
@@ -59,7 +59,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   const logout = async () => {
-    Cookies.remove("token", { domain: "localhost" });
+    Cookies.remove("_vercel_jwt", { domain: "localhost" });
     setIsAuthenticated(false);
     setUser(null);
   };
