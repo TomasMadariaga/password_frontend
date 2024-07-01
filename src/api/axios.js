39acd94c16ed377fa55d1d4c16a98ad09b2c22bb
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-07-01 17:36:34 -0300

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://password-backend-6g1b.onrender.com",
+  baseURL: "https://password-backend-6g1b.onrender.com/",
   // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
