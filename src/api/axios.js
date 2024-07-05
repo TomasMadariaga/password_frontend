79cc16aac735d5d83e693209a8ab8eee6909e799
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-07-05 19:16:59 -0300

    baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "https://password-backend-tomas.vercel.app/",
+  baseURL: "https://password-backend-tomas.vercel.app",
   // baseURL: "http://localhost:3000",
 });
 
